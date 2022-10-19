dd451ee6d3549940ddac8e72d4a90ba7546fb7e3
Alex Seifert <alex@alexseifert.com>
2022-10-19 22:26:19 +0200

    The title bar now looks nice

---
+++ windows/InOrdnung/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using InOrdnung.Views;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace InOrdnung
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            SetCurrentNavigationViewItem(args.SelectedItemContainer as NavigationViewItem);
+        }
+
+        private void SetCurrentNavigationViewItem(NavigationViewItem item)
+        {
+            if (item == null)
+            {
+                return;
+            }
+
+            if (item.Tag == null)
+            {
+                return;
+            }
+
+            ContentFrame.Navigate(Type.GetType(item.Tag.ToString()), item.Content);
+            NavigationView.Header = item.Content;
+        }
+
+        private void NavigationView_Loaded(object sender, RoutedEventArgs e)
+        {
+            SetCurrentNavigationViewItem(GetNavigationViewItems(typeof(ProjectInfo)).First());
+        }
+
+        public List<NavigationViewItem> GetNavigationViewItems()
+        {
+            var result = new List<NavigationViewItem>();
+            var items = NavigationView.MenuItems.Select(i => (NavigationViewItem)i).ToList();
+            items.AddRange(NavigationView.FooterMenuItems.Select(i => (NavigationViewItem)i));
+            result.AddRange(items);
+
+            foreach (NavigationViewItem mainItem in items)
+            {
+                result.AddRange(mainItem.MenuItems.Select(i => (NavigationViewItem)i));
+            }
+
+            return result;
+        }
+
+        public List<NavigationViewItem> GetNavigationViewItems(Type type)
+        {
+            return GetNavigationViewItems().Where(i => i.Tag.ToString() == type.FullName).ToList();
+        }
+
+        public List<NavigationViewItem> GetNavigationViewItems(Type type, string title)
+        {
+            return GetNavigationViewItems(type).Where(ni => ni.Content.ToString() == title).ToList();
+        }
+    }
+}
--- windows/InOrdnung/MainWindow.xaml.cs
+++ windows/InOrdnung/MainWindow.xaml.cs
@@ -1,18 +1,10 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using InOrdnung.Views;
+using System.Runtime.InteropServices;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -31,62 +23,5 @@ namespace InOrdnung
             ExtendsContentIntoTitleBar = true;
             SetTitleBar(AppTitleBar);
         }
-
-        //private void myButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    myButton.Content = "Clicked";
-        //}
-
-        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
-        {
-            SetCurrentNavigationViewItem(args.SelectedItemContainer as NavigationViewItem);
-        }
-
-        private void SetCurrentNavigationViewItem(NavigationViewItem item)
-        {
-            if (item == null)
-            {
-                return;
-            }
-
-            if (item.Tag == null)
-            {
-                return;
-            }
-
-            ContentFrame.Navigate(Type.GetType(item.Tag.ToString()), item.Content);
-            NavigationView.Header = item.Content;
-        }
-
-        private void NavigationView_Loaded(object sender, RoutedEventArgs e)
-        {
-            SetCurrentNavigationViewItem(GetNavigationViewItems(typeof(ProjectInfo)).First());
-        }
-
-        public List<NavigationViewItem> GetNavigationViewItems()
-        {
-            var result = new List<NavigationViewItem>();
-            var items = NavigationView.MenuItems.Select(i => (NavigationViewItem)i).ToList();
-            items.AddRange(NavigationView.FooterMenuItems.Select(i => (NavigationViewItem)i));
-            result.AddRange(items);
-
-            foreach (NavigationViewItem mainItem in items)
-            {
-                result.AddRange(mainItem.MenuItems.Select(i => (NavigationViewItem)i));
-            }
-
-            return result;
-        }
-
-        public List<NavigationViewItem> GetNavigationViewItems(Type type)
-        {
-            return GetNavigationViewItems().Where(i => i.Tag.ToString() == type.FullName).ToList();
-        }
-
-        public List<NavigationViewItem> GetNavigationViewItems(Type type, string title)
-        {
-            return GetNavigationViewItems(type).Where(ni => ni.Content.ToString() == title).ToList();
-        }
-
     }
 }
